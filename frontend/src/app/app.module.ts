e2f499c254011cfdec2724dc204742db54c11343
José <jose.rubens.veiga@gmail.com>
2020-09-04 22:01:11 -0300

    removendo ngrx

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
-import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './counter.reducer';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -16,8 +13,6 @@ import { counterReducer } from './counter.reducer';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-
-    StoreModule.forRoot({ count: counterReducer }),
   ],
   providers: [],
   bootstrap: [AppComponent],
