dd226ce25ce48108d22ae7f2da66554e76814182
José <jose.rubens.veiga@gmail.com>
2020-09-04 21:58:50 -0300

    criando o backend do projeto em node

---
+++ backend/src/models/Project.ts
@@ -0,0 +1,8 @@
+export interface Project {
+  id: number;
+  title: string;
+  text: string;
+  budget: number;
+  initialDate: string;
+  finalDate: string;
+}
---
+++ backend/src/routes/index.ts
@@ -0,0 +1,99 @@
+import * as express from 'express';
+import * as cors from 'cors';
+import { Project } from '../models/Project';
+
+const routes = express();
+routes.use(express.json());
+routes.use(cors());
+
+let lastId = 1;
+
+const projects: Project[] = [
+  {
+    id: 1,
+    title: 'd1231',
+    text: 'dasdas',
+    budget: 10,
+    initialDate: 'dasdas',
+    finalDate: 'dsadas',
+  },
+];
+
+routes.get('/projects', (req, res) => {
+  res.json({ projects });
+});
+
+routes.get('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const project = projects.find((p) => p.id === Number(id));
+
+  if (!project) {
+    return res.status(400).json({
+      message: 'Projeto não encontrado.',
+    });
+  }
+
+  return res.status(200).json({
+    project,
+  });
+});
+
+routes.post('/projects', (req, res) => {
+  const { budget, finalDate, initialDate, text, title } = req.body as Project;
+
+  const newProject = {
+    id: ++lastId,
+    title,
+    text,
+    budget,
+    finalDate,
+    initialDate,
+  };
+
+  projects.push(newProject);
+
+  return res.status(200).json({
+    message: 'Projeto adicionado com sucesso!',
+  });
+});
+
+routes.put('/projects/:id', (req, res) => {
+  const { budget, finalDate, initialDate, text, title } = req.body as Project;
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex((p) => p.id === Number(id));
+  const project = projects[projectIndex];
+
+  projects[projectIndex] = {
+    ...project,
+    budget,
+    finalDate,
+    initialDate,
+    text,
+    title,
+  };
+
+  return res.status(200).json({
+    message: 'Projeto atualizado com sucesso!',
+  });
+});
+
+routes.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const projectIndex = projects.findIndex((p) => p.id === Number(id));
+
+  if (projectIndex < 0) {
+    return res.status(400).json({
+      message: 'Projeto não encontrado.',
+    });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(200).json({
+    message: 'Projeto excluído com sucesso!',
+  });
+});
+
+export default routes;
