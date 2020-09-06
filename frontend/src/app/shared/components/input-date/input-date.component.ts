a2682d1ec62a7cf32feb2016eb9cdcc62c19efb6
José <jose.rubens.veiga@gmail.com>
2020-09-06 14:17:21 -0300

    criando o input -date component

---
+++ frontend/src/app/shared/components/input-date/input-date.component.ts
@@ -0,0 +1,72 @@
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Input,
+  Optional,
+  Self,
+} from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-date',
+  templateUrl: './input-date.component.html',
+  styleUrls: ['./input-date.component.scss'],
+})
+export class InputDateComponent
+  implements OnInit, AfterViewInit, ControlValueAccessor {
+  @Input() label: string;
+  @Input() placeholder: string;
+  @Input() type = 'text';
+
+  control = new FormControl();
+
+  onChange: (_: any) => void = (_: any) => {};
+  onTouched: () => void = () => {};
+
+  get value(): any {
+    return this.control.value;
+  }
+
+  set value(value: any) {
+    this.control.setValue(value);
+  }
+
+  constructor(@Optional() @Self() public ngControl: NgControl) {
+    if (this.ngControl) {
+      this.ngControl.valueAccessor = this;
+    }
+
+    this.control.valueChanges.subscribe((value) => {
+      this.onChange(value);
+    });
+  }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    if (this.control) {
+      this.control.setValidators(this.ngControl.validator);
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.control.disable();
+    } else {
+      this.control.enable();
+    }
+  }
+
+  writeValue(value: any): void {
+    this.control.setValue(value);
+  }
+}
