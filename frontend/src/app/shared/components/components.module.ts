8aaec97277a1abffd557563f9f96f1b9293617f1
José <jose.rubens.veiga@gmail.com>
2020-09-06 14:19:47 -0300

    criando o serviço de notificação

--- frontend/src/app/shared/components/components.module.ts
+++ frontend/src/app/shared/components/components.module.ts
@@ -10,9 +10,23 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { ToolbarComponent } from './toolbar';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { InputFieldComponent } from './input-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { TextAreaFieldComponent } from './text-area-field/text-area-field.component';
+import { InputDateComponent } from './input-date/input-date.component';
+
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [ToolbarComponent, SidenavComponent],
+  declarations: [
+    ToolbarComponent,
+    SidenavComponent,
+    InputFieldComponent,
+    TextAreaFieldComponent,
+    InputDateComponent,
+  ],
   imports: [
     CommonModule,
 
@@ -21,9 +35,20 @@ import { SidenavComponent } from './sidenav/sidenav.component';
     MatSidenavModule,
     MatListModule,
     MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
+    ReactiveFormsModule,
 
     RouterModule,
   ],
-  exports: [ToolbarComponent, SidenavComponent],
+  exports: [
+    ToolbarComponent,
+    SidenavComponent,
+    InputFieldComponent,
+    TextAreaFieldComponent,
+    InputDateComponent,
+  ],
 })
 export class ComponentsModule {}
