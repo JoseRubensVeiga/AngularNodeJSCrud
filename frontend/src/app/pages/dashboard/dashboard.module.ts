9adeb5f9e86a9b22df1739f2f782581358e8b1bc
José <jose.rubens.veiga@gmail.com>
2020-09-06 14:17:11 -0300

    criando a tela de dashboard

--- frontend/src/app/pages/dashboard/dashboard.module.ts
+++ frontend/src/app/pages/dashboard/dashboard.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    DashboardRoutingModule,
+    MatButtonModule,
+    MatListModule,
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
